95a36e672c80d0275a60eef4d18191913991eeb5
wesleydcm <wesleycostamatos@gmail.com>
2026-08-16 07:48:10 -0300

    feat: add SuppliersModule import to SearchModule

--- api/src/search/search.module.ts
+++ api/src/search/search.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
-import { SearchService } from './search.service';
-import { SearchController } from './search.controller';
+import { Module } from "@nestjs/common";
+import { SearchService } from "./search.service";
+import { SearchController } from "./search.controller";
+import { SuppliersModule } from "src/suppliers/suppliers.module";
 
 @Module({
+  imports: [SuppliersModule],
+  controllers: [SearchController],
   providers: [SearchService],
-  controllers: [SearchController]
 })
 export class SearchModule {}
